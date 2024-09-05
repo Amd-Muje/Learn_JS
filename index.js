f20176e1bb6429f24d16a4ed219093aba4b6b42e
Amd-Muje <ahmadmujahid343@gmail.com>
2024-09-05 09:19:47 +0800

    Day 1

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log(`Hello`);
+console.log(`I Like Pizza!`);
+
+window.alert(`This Is An Alert!`);
+window.alert(`i Like pizza`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `World `;
+
+//This is a Comment
+
+/* 
+long comment
+*/
