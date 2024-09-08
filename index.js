8741a52bd8649726c6f0ac32f7e2a64bf26ea0af
Amd-Muje <ahmadmujahid343@gmail.com>
2024-09-09 06:49:31 +0800

    Arithmetic

--- index.js
+++ index.js
@@ -2,24 +2,47 @@
 //let price = 20;
 //let gpa = 3.88;
 
-let firstName = "Alex";
+// let firstName = "Alex";
 
 //booleans
-let online = true;
+// let online = true;
 
-let fullName = "Wow keren";
-let age = "";
-let isStudent = true;
+// let fullName = "Wow keren";
+// let age = "";
+// let isStudent = true;
 
 
-console.log(typeof gpa);
-console.log(`You Are ${age} years old`);
-console.log(`The price is Rp.${prcie}K`);
-console.log(`My GPA ${gpa}`);
-console.log(`Your name is ${firstName}`);
-console.log(typeof online); // untuk lihat type datanya
-console.log(`Online ? : ${online}`);
+// console.log(typeof gpa);
+// console.log(`You Are ${age} years old`);
+// console.log(`The price is Rp.${prcie}K`);
+// console.log(`My GPA ${gpa}`);
+// console.log(`Your name is ${firstName}`);
+// console.log(typeof online); // untuk lihat type datanya
+// console.log(`Online ? : ${online}`);
 
-document.getElementById("p1").textContent = `your name is ${fullName}`;
-document.getElementById("p2").textContent = `you are ${age} years old`;
-document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+// document.getElementById("p1").textContent = `your name is ${fullName}`;
+// document.getElementById("p2").textContent = `you are ${age} years old`;
+// document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
+
+// arithmetic operators = operands ( values, var, etc.)
+//                         operators (+ - * /)
+
+
+let students = 20;
+
+students = students ** 2 // perpangkatan
+let extraStudents = students % 3;
+
+console.log(students);
+
+students += 1;
+students -= 1;
+students *= 2;
+
+//increment and decrement
+
+students++;
+students--;
+
+console.log(students);
