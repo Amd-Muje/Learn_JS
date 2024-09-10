487f0246132bbcae15082a7be20686f4c6b709b5
Amd-Muje <ahmadmujahid343@gmail.com>
2024-09-11 04:19:53 +0800

    const

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-// type conversion 
+// const = a variable that can't be changed
+
+const PI = 3.14159;
+let radius;
+let circumference;
 
 
-let age = window.prompt("How Old are you ?");
-age = Number(age);
-age+=1;
+console.log(circumference);
 
-console.log(age, typeof age);
+document.getElementById("mySubmit").onclick = function() {
+    radius = document.getElementById("myText").value
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById("myH3").textContent = circumference + "cm";
 
-let x = "pizza";
-let y = "pizza";
-let z = "pizza";
-
-x = Number(x);
-y = String(y);
-z = Boolean(z);
-
-console.log(x, typeof x);
-console.log(y, typeof y);
-console.log(z, typeof z);
-
+}
