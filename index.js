dfa8aa6987a12f89e12a9fc0c00810a013f767a1
Amd-Muje <ahmadmujahid343@gmail.com>
2024-09-13 06:39:20 +0800

    Counter Program

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-// const = a variable that can't be changed
+//Counter Program
 
-const PI = 3.14159;
-let radius;
-let circumference;
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
 
+increaseBtn.onclick = function() {
+    count++;
+    countLabel.textContent = count;
+}
 
-console.log(circumference);
+decreaseBtn.onclick = function() {
+    count--;
+    countLabel.textContent = count;
+}
 
-document.getElementById("mySubmit").onclick = function() {
-    radius = document.getElementById("myText").value
-    radius = Number(radius);
-    circumference = 2 * PI * radius;
-    document.getElementById("myH3").textContent = circumference + "cm";
-
+resetBtn.onclick = () => {
+    count = 0;
+    countLabel.textContent = count;
 }
