b9ec8512bdf6e898bf8536f5c39dc2616accb96e
Amd-Muje <ahmadmujahid343@gmail.com>
2024-09-14 21:57:53 +0800

    Random Number Generator

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-// math
+// RANDOM NUMBER GENERATOR
 
-let x = 3.99;
-let y = 2;
-let z = 1;
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const min = 1;
+const max = 6;
+let randomNum;
 
-// z = Math.round(x);
-// z = Math.ceil(x);
-// z = Math.floor(x);
-// z = Math.trunc(x);
-// z = Math.pow(x,y);
-// z = Math.sqrt(x);
-// z = Math.log(x);
-// z = Math.sin(x);
-// z = Math.cos(x);
-// z = Math.tan(x);
-// z = Math.abs(x);
-// z = Math.sign(x);
-let max = Math.max(x, y, z);
-let min = Math.min(x, y, z);
-
-console.log(z);
+myButton.onclick = function() {
+    randomNum = Math.floor(Math.random() * max) + min ;
+    myLabel.textContent = randomNum;
+}
