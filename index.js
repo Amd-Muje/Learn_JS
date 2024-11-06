15a2835793749913953547355c99ea143da8c5d3
Amd-Muje <ahmadmujahid343@gmail.com>
2024-11-06 11:37:16 +0800

    string method 2

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-// string methods
+// // string methods
 
-let username = " AhmadMujahid   ";
+// let username = " AhmadMujahid   ";
 
-console.log(username.charAt(2)); // melihat char apa di index tertentu
+// console.log(username.charAt(2)); // melihat char apa di index tertentu
 
-console.log(username.indexOf("A"));
+// console.log(username.indexOf("A"));
 
-console.log(username.length); // melihat panjang string
+// console.log(username.length); // melihat panjang string
 
-username = username.trim(); // untuk memotong white Space => spasi yang tidak jelas awal dan akhir
-username = username.toUpperCase(); // 
-username = username.toLowerCase(); // 
+// username = username.trim(); // untuk memotong white Space => spasi yang tidak jelas awal dan akhir
+// username = username.toUpperCase(); // 
+// username = username.toLowerCase(); // 
 
-console.log(username);
+// console.log(username);
 
-console.log(username.repeat(3)); // untuk mengulangi string sebanyak 3 kali
+// console.log(username.repeat(3)); // untuk mengulangi string sebanyak 3 kali
 
-let result = username.startsWith(" "); // untuk mengetahui true or false dari parameter yang diberikan
+// let result = username.startsWith(" "); // untuk mengetahui true or false dari parameter yang diberikan
 
-if (result) {
-    console.log("Your Username can't begin with ' '");
-} else {
-    console.log(username);
-}
+// if (result) {
+//     console.log("Your Username can't begin with ' '");
+// } else {
+//     console.log(username);
+// }
+
+let phoneNumber = "0812-3456-7890"
+
+phoneNumber = phoneNumber.replaceAll("-","+");
+
+console.log(phoneNumber);
+
+phoneNumber = phoneNumber.padStart(30,"-");
+
+console.log(phoneNumber);
