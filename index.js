2e2d96c15611b0260fd5f4011c7ea8a08bfa40ec
Amd-Muje <ahmadmujahid343@gmail.com>
2024-11-08 09:42:59 +0800

    String slice 2

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const fullname = "Ahmad Mujahid";
+// const fullname = "Ahmad Mujahid";
 
-// let firstName = fullname.slice(0, 5);
-// let lastName = fullname.slice(6, 13);
-
-
-let firstName = fullname.slice(0, fullname.indexOf(" "));
-let lastName = fullname.slice(fullname.indexOf(" ") + 1);
-
-console.log(firstName);
-console.log(lastName);
+// // let firstName = fullname.slice(0, 5);
+// // let lastName = fullname.slice(6, 13);
 
 
+// let firstName = fullname.slice(0, fullname.indexOf(" "));
+// let lastName = fullname.slice(fullname.indexOf(" ") + 1);
+
+// console.log(firstName);
+// console.log(lastName);
+
+const email = "Keren@gmail.com";
+
+let username = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@") + 1);
+
+console.log(username);
+console.log(extension);
